241bf2dc056ea5cb8cd09bc1d1201d8dcb57a7b9
Kieran <kdleschinski@gmail.com>
2019-11-26 15:37:26 +0100

    added skycalc_ipy to travis requirements

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def setup_package():
                             "pyyaml>3",
                             "synphot>0.1",
                             "pyckles",
-                            "scopesim"
+                            "scopesim",
                             ],
           classifiers=["Programming Language :: Python :: 3",
                        "License :: OSI Approved :: MIT License",
