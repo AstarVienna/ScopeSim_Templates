460e6eebf0d81670053737949afa36acd34f7126
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2020-07-22 15:09:32 +0200

    Fix flatlamp test

--- scopesim_templates/tests/test_micado/test_flatlamp.py
+++ scopesim_templates/tests/test_micado/test_flatlamp.py
@@ -1,5 +1,5 @@
 """Test for flatlamp."""
-from astropy.table import Table
+from astropy.io.fits import ImageHDU
 from synphot import SourceSpectrum
 from scopesim_templates.micado import flatlamp
 from scopesim_templates.rc import Source
@@ -10,8 +10,7 @@ class TestFlatlamp:
 
     def test_flatlamp_returns_source_object(self):
         """Test for flatlamp."""
-        sky = flatlamp()
-        assert isinstance(sky, Source)
-        assert isinstance(sky.spectra[0], SourceSpectrum)
-        assert isinstance(sky.fields[0], Table)
-        assert sky.fields[0]["ref"][0] == 0
+        lamp = flatlamp()
+        assert isinstance(lamp, Source)
+        assert isinstance(lamp.spectra[0], SourceSpectrum)
+        assert isinstance(lamp.fields[0], ImageHDU)
