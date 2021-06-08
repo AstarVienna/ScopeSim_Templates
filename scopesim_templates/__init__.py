34ba30fa8285735c735c88d8b7d85d9c0b6a62a8
Miguel Verdugo <mverduol@gmail.com>
2021-06-08 16:05:21 +0200

    making sources more accessible

--- scopesim_templates/__init__.py
+++ scopesim_templates/__init__.py
@@ -2,3 +2,10 @@ from . import advanced
 from . import basic
 from . import micado
 from . import utils
+
+
+from .basic.basic import source_from_image, source_from_file, source_from_cube
+from .basic.stars import star, stars, star_grid, star_field, cluster
+#from .basic.galaxy import galaxy, spiral_two_component
+
+#from .basic.misc import empty_sky, flat_field
