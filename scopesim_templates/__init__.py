3aa16b2aee4fbe24104d971dc3aba02201b77f2d
Kieran Leschinski <kdleschinski@gmail.com>
2021-06-09 09:12:03 +0200

    Update __init__.py
    removed  shortcuts to basic functions

--- scopesim_templates/__init__.py
+++ scopesim_templates/__init__.py
@@ -2,9 +2,9 @@ from . import basic
 from . import micado
 from . import utils
 
-from .basic.basic import source_from_image, source_from_file, source_from_cube
-from .basic.galaxy import galaxy, spiral_two_component
-from .basic.stars import star, stars, star_grid, star_field, cluster
-from .basic.misc import empty_sky, flat_field
+# from .basic.basic import source_from_image, source_from_file, source_from_cube
+# from .basic.galaxy import galaxy, spiral_two_component
+# from .basic.stars import star, stars, star_grid, star_field, cluster
+# from .basic.misc import empty_sky, flat_field
 
 
