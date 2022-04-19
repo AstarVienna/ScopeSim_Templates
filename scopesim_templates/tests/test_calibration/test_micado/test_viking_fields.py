187a5ab2275d61b5622d592561c8e8e48479c5eb
Kieran <kdleschinski@gmail.com>
2022-04-19 14:05:46 +0200

    testing of vikings field green =)

--- scopesim_templates/tests/test_calibration/test_micado/test_viking_fields.py
+++ scopesim_templates/tests/test_calibration/test_micado/test_viking_fields.py
@@ -6,6 +6,8 @@ from astropy import units as u
 from matplotlib import pyplot as plt
 from matplotlib.colors import LogNorm
 
+from scopesim import load_example_optical_train
+
 from scopesim_templates.calibration.micado import viking_fields as vf
 
 PLOTS = False
@@ -58,7 +60,7 @@ class TestVikingCatalogues:
         stars = viking_src.table_fields
         star_x, star_y = stars[0]["x"].data, stars[0]["y"].data
 
-        if not PLOTS:
+        if PLOTS:
             plt.figure(figsize=(10, 10))
             plt.scatter(gal_x, gal_y, c="b")
             plt.scatter(star_x, star_y, c="r")
@@ -66,3 +68,37 @@ class TestVikingCatalogues:
 
         assert len(viking_src.fields) > 1
         assert len(viking_src.fields) == len(gals) + len(stars)
+
+
+class TestWithScopeSim:
+    def test_runs_through_scopesim_basic_instrument(self):
+        # configure basic_instrument to MICADO specs
+        opt = load_example_optical_train()
+        opt.cmds["!OBS.psf_fwhm"] = 0.01
+        opt.cmds["!TEL.area"] = 1000 * u.m**2
+        opt.cmds["!INST.pixel_scale"] = 0.004
+        opt.cmds["!INST.plate_scale"] = 0.4
+        opt.cmds["!DET.width"] = 4096
+        opt.cmds["!DET.height"] = 4096
+        opt.cmds["!DET.dit"] = 30
+        opt.cmds["!DET.ndit"] = 120
+
+        opt["source_fits_keywords"].include = False
+
+        src = vf.viking_field()
+        opt.observe(src)
+        hdul = opt.readout()[0]
+
+        imp_im = opt.image_planes[0].data
+        imp_im += np.min(imp_im)
+
+        if PLOTS:
+            plt.figure(figsize=(15, 8))
+            plt.subplot(121)
+            plt.imshow(opt.image_planes[0].data, norm=LogNorm())
+            plt.subplot(122)
+            plt.imshow(hdul[1].data, norm=LogNorm())
+            plt.show()
+
+        assert isinstance(imp_im, np.ndarray)
+        assert np.max(imp_im) > 1
