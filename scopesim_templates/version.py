450eed53303ab9c466a9646c4aa19643b863555a
Kieran <kdleschinski@gmail.com>
2022-07-13 13:01:18 +0200

    updated version to 0.4.2

--- scopesim_templates/version.py
+++ scopesim_templates/version.py
@@ -1,6 +1,10 @@
-version = '0.4.1'
-date    = '2022-04-22 17:00:00 GMT'
+version = '0.4.2'
+date    = '2022-07-12 17:00:00 GMT'
 yaml_descriptions = """
+- version : 0.4.2
+  date : 2022-07-12
+  comment : Updated DLC server to point to scopesim.univie.ac.at
+
 - version : 0.4.1
   date : 2022-04-22
   comment : Updated package structure
