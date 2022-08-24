9de1fdc430d193c5567a50939990d4a6f8f33ff1
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2022-08-24 22:57:22 +0200

    Properly set meta["function_call"] of micado functions.

--- scopesim_templates/micado/__init__.py
+++ scopesim_templates/micado/__init__.py
@@ -1,9 +1,30 @@
-from .darkness import darkness
-from .flatlamp import flatlamp
-from .cluster import cluster
+from .darkness import darkness as _darkness
+from .flatlamp import flatlamp as _flatlamp
+from .cluster import cluster as _cluster
 from .viking_fields import viking_fields
-# Include empty_sky for backwards compatibility reasons.
-# TODO: Remove empty_sky when it is not used in MicadoWISE anymore.
-empty_sky = darkness
+from ..utils.general_utils import add_function_call_str
+
+
+@add_function_call_str
+def darkness():
+    _darkness()
+
+
+@add_function_call_str
+def empty_sky():
+    # Include empty_sky for backwards compatibility reasons.
+    # TODO: Remove empty_sky when it is not used in MicadoWISE anymore.
+    return _darkness()
+
+
+@add_function_call_str
+def flatlamp():
+    return _flatlamp()
+
+
+@add_function_call_str
+def cluster():
+    return _cluster()
+
 
 __all__ = [darkness, flatlamp, empty_sky, cluster]
