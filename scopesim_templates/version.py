655613e265db7d20c7559cb6595b21a4a01a14d5
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2023-03-08 19:21:45 +0100

    Prepare for 0.4.4 release

--- scopesim_templates/version.py
+++ scopesim_templates/version.py
@@ -1,6 +1,10 @@
-version = '0.4.3'
-date    = '2023-02-15 17:00:00 GMT'
+version = '0.4.4'
+date    = '2023-03-08 18:00:00 GMT'
 yaml_descriptions = """
+- version : 0.4.4
+  date : 2023-03-08
+  comment : Fix metadata bug in 2D galaxy.
+
 - version : 0.4.3
   date : 2023-02-15
   comment : Add pinhole mask and minor bugfixes
