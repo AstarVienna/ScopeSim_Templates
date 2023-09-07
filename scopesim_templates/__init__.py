31e4b0162e81d4ea73a8401aeca94ffc79e5a67f
teutoburg <ghost@instruct.at>
2023-09-07 19:35:07 +0200

    see what causes the warning

--- scopesim_templates/__init__.py
+++ scopesim_templates/__init__.py
@@ -8,7 +8,7 @@ from . import utils
 
 warnings.warn("In a future version top level function calls will be removed. "
               "Always use this syntax: from module.submodule import function",
-              DeprecationWarning)
+              DeprecationWarning, stacklevel=2)
 from .misc.misc import source_from_image, source_from_file, source_from_cube
 from .stellar.stars import star, stars, star_grid, star_field
 from .stellar.clusters import cluster
