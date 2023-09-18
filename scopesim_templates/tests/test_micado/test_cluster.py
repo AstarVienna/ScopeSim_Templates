889bb1c2ee4df8f1710bf5ecba421890bd56ff7e
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2023-09-18 12:00:27 +0200

    Use isinstance(x, t) instead of type(x)==t
    These checks were broken anyway, because they only checked for
    float, while the values where actually numpy.float64. As a result,
    many functions returned an an array with a single element. This
    array then got silently converted to a float anyway, but this will
    change, as this warning is emitted:
    
    DeprecationWarning: Conversion of an array with ndim > 0 to a scalar
    is deprecated, and will error in future. Ensure you extract a single
    element from your array before performing this operation.
    (Deprecated NumPy 1.25.)
    
    isintance will return True for both float and numpy.float64, so
    now everything will work as intended.
    
    Also removing a mutable array from a function definition.

---
+++ scopesim_templates/tests/test_micado/test_cluster.py
@@ -0,0 +1,17 @@
+"""Test cluster."""
+from astropy.table import Table
+from synphot import SourceSpectrum
+from scopesim_templates.micado import cluster
+from scopesim_templates.rc import Source
+import scopesim_templates
+
+
+class TestCluster:
+    """Test cluster."""
+
+    def test_cluster_returns_source_object(self):
+        """Test cluster."""
+        sky = cluster()
+        assert isinstance(sky, Source)
+        assert isinstance(sky.spectra[0], SourceSpectrum)
+        assert isinstance(sky.fields[0], Table)
