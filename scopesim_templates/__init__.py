a0dd0982eb04effcaf30f30a004787873adbcd8c
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2023-10-04 08:20:00 +0200

    Disable warning on input of scopesim_templates

--- scopesim_templates/__init__.py
+++ scopesim_templates/__init__.py
@@ -6,6 +6,10 @@ from . import misc
 from . import stellar
 from . import utils
 
+# This warning is emitted when just doing "import scopesim_templates", which
+# should be a normal non-warning thing to do.
+# TODO: Find a way to emit this warning only when the functions below are
+#       accessed directly.
 warnings.warn("In a future version top level function calls will be removed. "
               "Always use this syntax: from module.submodule import function",
               DeprecationWarning, stacklevel=2)
