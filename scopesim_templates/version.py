387681fd9102d1844787773c1116e2cbd68870af
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2023-10-19 09:28:46 +0200

    Preparation for release of v0.5.0

--- scopesim_templates/version.py
+++ scopesim_templates/version.py
@@ -1,10 +1,14 @@
 from importlib import metadata
 version = metadata.version(__package__)
-date = '2023-03-08 18:00:00 GMT'
+date = '2023-10-19 08:00:00 GMT'
 yaml_descriptions = """
-- version : 0.4.5
-  date : xxxx-xx-xx
-  comment : Switched to pyproject.toml
+- version : 0.5.0
+  date : 2023-10-19
+  comment : Fix off-by-one error.
+  changes :
+  - Fix off-by-one error.
+  - Use pyproject.toml.
+  - Various cleanups and fixes.
 
 - version : 0.4.4
   date : 2023-03-08
