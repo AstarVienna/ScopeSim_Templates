bd25176b4b6c80ea6b4c61750baf2eb2d96b4f12
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2024-10-28 20:28:31 +0100

    Fix and test calibration module

--- scopesim_templates/calibration/calibration.py
+++ scopesim_templates/calibration/calibration.py
@@ -6,15 +6,17 @@ import warnings
 import numpy as np
 from astropy import units as u
 
-from spextra import Spextrum
+from spextra import Spextrum, Passband
 
 from ..rc import Source, __config__
 from ..utils.general_utils import add_function_call_str
 from ..misc.misc import uniform_source
 
-__all__ = ["lamp",
-           "flat_field",
-           "empty_sky"]
+__all__ = [
+    "lamp",
+    "flat_field",
+    "empty_sky",
+]
 
 
 @add_function_call_str
@@ -62,7 +64,10 @@ def lamp(waves, fwhm, fluxes) -> Source:
 
     w_minmax = [waves.min().value, waves.max().value] * u.AA
     # A very faint spextrum to add the lines
-    spec = Spextrum.flat_spectrum(amplitude=40, waves=w_minmax)
+    spec = Spextrum.flat_spectrum(
+        amplitude=40 * u.ABmag,
+        waves=w_minmax,
+    )
     spec = spec.add_emi_lines(center=waves, fwhm=fwhm, flux=fluxes)
     src = uniform_source(sed=spec)
     # src.meta.update({"object": "lamp"})
@@ -71,8 +76,12 @@ def lamp(waves, fwhm, fluxes) -> Source:
 
 
 @add_function_call_str
-def flat_field(temperature=5000, amplitude=0*u.ABmag, filter_curve="V",
-               extend=60) -> Source:
+def flat_field(
+    temperature: u.Quantity[u.K] = 5000 * u.K,
+    amplitude: u.Quantity[u.ABmag] = 0 * u.ABmag,
+    filter_curve: str | Passband = "V",
+    extend=60,
+) -> Source:
     """
     Create a flat-field source to be used in ``ScopeSim``.
 
@@ -87,9 +96,9 @@ def flat_field(temperature=5000, amplitude=0*u.ABmag, filter_curve="V",
 
     Parameters
     ----------
-    temperature: float
+    temperature: ``astropy.Quantity``
         [Kelvin] Temperature of the lamp
-    amplitude: u.Quantity
+    amplitude: ``astropy.Quantity``
         [u.Quantity] amplitude of the lamp in u.ABmag, u.mag (vega) or u. STmag
     filter_curve: str
         any filter curve available for ``spextra``, used for scaling
@@ -98,17 +107,22 @@ def flat_field(temperature=5000, amplitude=0*u.ABmag, filter_curve="V",
     Returns
     -------
     src: Source
-
     """
     # I don't understand this warning but it probably has a reason...
     warnings.warn("Make sure to turn off the appropriate ScopeSim effects "
                   "during the simulation", RuntimeWarning)
 
-    spec = Spextrum.black_body_spectrum(temperature=temperature,
-                                        amplitude=amplitude,
-                                        filter_curve=filter_curve)
-    src = uniform_source(sed=spec, amplitude=amplitude,
-                         filter_curve=filter_curve, extend=extend)
+    spec = Spextrum.black_body_spectrum(
+        temperature=temperature,
+        amplitude=amplitude,
+        filter_curve=filter_curve,
+    )
+    src = uniform_source(
+        sed=spec,
+        amplitude=amplitude,
+        filter_curve=filter_curve,
+        extend=extend,
+    )
     # src.meta.update({"object": "flat_field"})
 
     return src
