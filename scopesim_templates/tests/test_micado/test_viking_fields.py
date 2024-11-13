7ff79d8ac362c66843e4d82f7382e3fce7140cd9
teutoburg <73600109+teutoburg@users.noreply.github.com>
2024-11-13 14:04:12 +0100

    Didn't see the second one here...
    Co-authored-by: Hugo Buddelmeijer <hugo@buddelmeijer.nl>

--- scopesim_templates/tests/test_micado/test_viking_fields.py
+++ scopesim_templates/tests/test_micado/test_viking_fields.py
@@ -45,7 +45,6 @@ class TestVikingCatalogues:
         assert len(star_src.fields) == 1
         assert len(star_src.fields[0]) > 1
         assert len(star_src.spectra) == 1
-        # assert len(star_src.fields) == len(star_src._meta_dicts)
 
     def test_load_viking_field(self):
         viking_src = vf.viking_field()
