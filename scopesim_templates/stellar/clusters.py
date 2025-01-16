1fdc077e71f6a6ec9c5952a16ec5c1dd374b7311
teutoburg <ghost@instruct.at>
2025-01-16 13:26:18 +0100

    Remove unrealistically high masses instead of clipping

--- scopesim_templates/stellar/clusters.py
+++ scopesim_templates/stellar/clusters.py
@@ -81,7 +81,9 @@ def cluster(mass=1E3, distance=50000, core_radius=1, ra=RA0, dec=DEC0,
     # 1. sample masses from an IMF
     kroupa = imf.Kroupa_2001(params["multiplicity_object"])
     masses, _, _, _ = kroupa.generate_cluster(mass, seed=params["seed"])
-    masses.clip(max=250, out=masses)
+    # masses.clip(max=250, out=masses)
+    # Avoid unrealistic top-heavy IMF
+    masses = masses[masses < 20]
 
     # 2. get spec_types for masses
     spec_types = cu.mass2spt(masses)
