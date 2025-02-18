45fed72a4e9dc8918ae20e8910980a0318148888
teutoburg <ghost@instruct.at>
2025-02-18 22:59:27 +0100

    Remove defaults.yaml and just spell out the dict
    There were only three values in the default.yaml, this is unnecessarily
    complex, just put the dict in rc.py directly.

--- scopesim_templates/rc.py
+++ scopesim_templates/rc.py
@@ -1,6 +1,5 @@
-from pathlib import Path
-import sys
-import yaml
+# -*- coding: utf-8 -*-
+"""Store default values and keep ScopeSim imports in one place."""
 
 from astar_utils import NestedMapping
 
@@ -12,6 +11,12 @@ im_plane_utils = scopesim.optics.image_plane_utils
 scopesim_utils = scopesim.utils
 load_example_optical_train = scopesim.load_example_optical_train
 
-PKG_DIR = Path(__file__).parent
-with (PKG_DIR / "defaults.yaml").open(encoding="utf-8") as file:
-    __config__ = NestedMapping(yaml.full_load(file))
+__config__ = NestedMapping({
+    "spectral": {
+        "wave_min": 0.3,
+        "wave_max": 3.0,
+    },
+    "random": {
+        "seed": 9001,
+    },
+})
